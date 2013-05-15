4c6a74b5b27572ceedea46b555504a8916ea858f
KallDrexx <me@mshapiro.net>
2013-05-14 22:28:53 -0400

    Added a built-in alias to handle hex values

---
+++ RapidRegex.Core/Aliases/Misc.cs
@@ -0,0 +1,16 @@
+﻿namespace RapidRegex.Core.Aliases
+{
+    public static class Misc
+    {
+        public static  RegexAlias[] Aliases
+        {
+            get
+            {
+                return new[]
+                {
+                    new RegexAlias {Name = "HEX", RegexPattern = @"#?([a-f0-9]{6}|[a-f0-9]{3})\b"}
+                };
+            }
+        }
+    }
+}
---
+++ RapidRegex.Tests/MiscAliasTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+using NUnit.Framework;
+using RapidRegex.Core;
+using RapidRegex.Core.Aliases;
+
+namespace RapidRegex.Tests
+{
+    [TestFixture]
+    public class MiscAliasTests
+    {
+        [Test]
+        public void Has_Alias_For_Hex_Values()
+        {
+            var aliases = Misc.Aliases;
+            var resolver = new RegexAliasResolver(aliases);
+            var realPattern = resolver.ResolveToRegex("%{HEX}");
+
+            var match1 = Regex.Match("#a3c113", realPattern);
+            var match2 = Regex.Match("#4d82h4", realPattern);
+
+            Assert.IsTrue(match1.Success, "Valid hex value was not matched");
+            Assert.IsFalse(match2.Success, "Invalid hex value was incorrectly matched");
+        }
+    }
+}
